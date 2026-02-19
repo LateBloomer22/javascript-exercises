6dc84a3da735cd62c1426d7fbdb3607ae71bdcc0
LateBloomer22 <23632544+LateBloomer22@users.noreply.github.com>
2026-02-19 21:03:41 +0530

    JS 9th assignment

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    if ((Number.isInteger(a) && Number.isInteger(b)) && (a >= 0 && b >= 0)){
+        let lowerBound = a;
+        let upperBound = b;
+        let sum = 0;
+        if (a > b) {
+            lowerBound = b;
+            upperBound = a; }
+        // } else {
+        //     lowerBound = a;
+        //     upperBound = b;
+        // }
 
+        for (let i = lowerBound; i <= upperBound; i++){
+            sum += i;
+        }
+        return sum; 
+    } else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
