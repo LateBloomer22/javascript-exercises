4702ec870b44f419d9f66f54f4d03af61290ce62
LateBloomer22 <23632544+LateBloomer22@users.noreply.github.com>
2026-03-27 17:05:01 +0530

    JS assignment

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (inputText) {
+    let newtext = inputText.replace(/[^\w]/g,'').toLowerCase();
+    let reversedText = "";
+    for (let i = newtext.length; i > 0 ; i--){
+        reversedText = reversedText + newtext[i-1];
+    }
+    if (reversedText == newtext){
+        return true
+    } else return false;
 };
 
 // Do not edit below this line
