5b079dec718389046f85b4b6c9c9c568f8f3d173
LateBloomer22 <23632544+LateBloomer22@users.noreply.github.com>
2026-03-27 15:18:22 +0530

    JS assignment

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((accumulator, currentValue) => accumulator*currentValue, 1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let i = 1;
+  if (num == 1 || num == 0){
+    // return i;
+  } else {
+    for (num; num > 1; num--){
+      i = i*num;
+    }
+  }
+  return i;
 };
 
 // Do not edit below this line
